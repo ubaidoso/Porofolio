87d6e9c38718b2d14ddf69829401d401dc8b8f59
ubaidoso <81627418+ubaidoso@users.noreply.github.com>
2024-07-26 02:51:06 +0500

    resp
    in process

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,23 +7,22 @@ const Experience = ({experienceData}) => {
     <section className='my-24'>
         <div className="container mx-auto py-8">
         <div className="flex justify-between gap-8">
-            <div className="md:w-5/12 w-6/12">
+            <div className="md:w-6/12 w-6/12">
             <div className="space-y-8">
                 {experienceData.map((experienceItem, index) => (
                 <div key={index} className="space-y-8 border-b border-black pb-8">
-                    <div className="space-y-3">
-                        <p className="uppercase text-sm font-normal tracking-widest leading-6 text-lightblack">{experienceItem.designation}</p>
+                    <div className="lg:space-y-3 space-y-2">
+                        <p className="uppercase lg:text-sm text-xs font-normal tracking-widest leading-6 text-lightblack">{experienceItem.designation}</p>
                     <div className="flex items-center justify-between">
-                        
-                    <h2 className="font-semibold md:text-2xl text-lg">{experienceItem.company}</h2>
-                    <h3 className="font-normal text-sm ">{experienceItem.duration}</h3>
+                        <h2 className="font-semibold lg:text-2xl text-base">{experienceItem.company}</h2>
+                        <h3 className="font-normal text-sm ">{experienceItem.duration}</h3>
                     </div>
                     </div>
                 </div>
                 ))}
             </div>
             </div>
-            <div className="space-y-8 md:w-6/12 w-5/12 sticky top-16 self-start">
+            <div className="space-y-8 md:w-5/12 w-5/12 sticky top-16 self-start">
             <h2>My Experiences</h2>
             <div className='space-y-3'>
                 <h3 className="font-medium text-lg">Lets work together, I am open for part time / freelance</h3>
