1a0b1f457f1da6368ef92a584953e5beadd53680
ubaidoso <81627418+ubaidoso@users.noreply.github.com>
2024-07-30 03:34:41 +0500

    efr
    dsfg

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,10 +7,10 @@ function App({ Component, pageProps }) {
 
   const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => setLoading(false), 5000);
-  //   return () => clearTimeout(timer);
-  // }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 5000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
